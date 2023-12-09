428c1403dd534bcd2b47c2248d31990087903d8e
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-09 20:24:19 +0200

    2nd update

--- khedira_print.c
+++ khedira_print.c
@@ -1,7 +1,7 @@
 #include "khedira_shell.h"
 
 /**
-* khedira_print - a function that prints messages to the standard output
+* khedira - a function that prints messages to the standard output
 * Return: Void
 * @output: the message that we want to print.
 */
