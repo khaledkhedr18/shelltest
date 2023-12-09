5685ca24dd3b4f2ef72b6253ed3a2dc2eb7e4183
Khaled Khedr <khaledkhedr18@gmail.com>
2023-12-09 16:47:30 +0000

    1st update to the shell

---
+++ khedira_shell.h
@@ -0,0 +1,13 @@
+#ifndef KHEDIRA_SHELL_H
+#define KHEDIRA_SHELL_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void khedira_prompt();
+
+#endif
