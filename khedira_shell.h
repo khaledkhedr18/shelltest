02c342b211c109769857c05537e6dd73117a7ba9
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-09 21:35:15 +0200

    ss

--- khedira_shell.h
+++ khedira_shell.h
@@ -17,7 +17,7 @@ extern char **environ;
 void khedira_prompt();
 void khedira(const char *output);
 void khedira_uinput(char *comm, size_t size);
-void khedira_exec(char **exec_comm, char **exec_args);
+void khedira_exec(char *exec_comm, char *args[]);
 int khedira_tokenize(char *myinp, char *myargs[]);
 
 #endif
