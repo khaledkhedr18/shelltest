df845dad6926374433786425f86edeeeeb129869
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-10 13:56:33 +0200

    ss

--- khedira_exec.c
+++ khedira_exec.c
@@ -10,6 +10,13 @@
 int khedira_exec(char *exec_comm, char *exec_args[], int mystatus)
 {
 	pid_t pid = fork();
+	char *command[4];
+
+	command[0] = "/bin/sh";
+	command[1] = "-c";
+	command[2] = (char *)exec_comm;
+	command[3] = NULL;
+
 
 	if (pid == -1)
 	{
@@ -19,7 +26,7 @@ int khedira_exec(char *exec_comm, char *exec_args[], int mystatus)
 
 	else if (pid == 0)
 	{
-		execve(exec_comm, exec_args, environ);
+		execve(command[0], exec_args, environ);
 		perror(exec_args[0]);
 		exit(EXIT_FAILURE);
 	}
