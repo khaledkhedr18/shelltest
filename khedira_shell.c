1bf98018925b966fe8c95032cab779c9fe2f996a
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-10 14:25:52 +0200

    ss

--- khedira_shell.c
+++ khedira_shell.c
@@ -8,9 +8,12 @@
 int main()
 {
 	size_t input_len;
-	int myargcount;
 	char myInput[MAX_ARGINPUT_SIZE];
-	char *myargs[MAX_ARGUMENTS];
+	/**
+	* int myargcount;
+
+	* char *myargs[MAX_ARGUMENTS];
+	*/
 	int mystatus = 0;
 
 	while (1)
@@ -21,26 +24,7 @@ int main()
 		}
 
 		khedira_uinput(myInput, sizeof(myInput), mystatus);
-		input_len = strlen(myInput);
-
-		if (input_len > 0 && myInput[input_len - 1] == '\n')
-		{
-			myInput[input_len - 1] = '\0';
-		}
-
-
-		myargcount = khedira_tokenize(myInput, myargs);
-
-		if (myargcount > 0)
-		{
-			if (khedira_exit(myargs[0]))
-			break;
-
-			else
-			{
-				mystatus = khedira_exec(myargs[0], mystatus);
-			}
-		}
+		khedira_exec(myInput, mystatus);
 	}
 	return (0);
 }
