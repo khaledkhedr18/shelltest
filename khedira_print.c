59f0e8cf90e2ad544111c80d83820ef89c0a3e33
khaledkhedr18 <khaledkhedr18@gmail.com>
2023-12-10 17:07:41 +0200

    ss

--- khedira_print.c
+++ khedira_print.c
@@ -5,16 +5,16 @@
  * @myChar: the character to be printed
 */
 
-int	khedira_char(int myChar) 
+int	khedira_char(int myChar)
 {
-	return write(1, &myChar, 1);
+	return (write(1, &myChar, 1));
 }
 
 /**
  * khedira_digit - a function that prints digits
  * Return: int
  * @x: a variable
- * myBase: the base
+ * @myBase: the base
 */
 
 int	khedira_digit(long x, int myBase)
@@ -26,14 +26,14 @@ int	khedira_digit(long x, int myBase)
 	if (x < 0)
 	{
 		write(1, "-", 1);
-		return khedira_digit(-x, myBase) + 1;
+		return (khedira_digit(-x, myBase) + 1);
 	}
 	else if (x < myBase)
-		return khedira_char(symbols[x]);
+		return (khedira_char(symbols[x]));
 	else
 	{
 		count = khedira_digit(x / myBase, myBase);
-		return count + khedira_digit(x % myBase, myBase);
+		return (count + khedira_digit(x % myBase, myBase));
 	}
 }
 
@@ -50,7 +50,7 @@ int	khedira_str(char *myString)
 	count = 0;
 	while (*myString)
 		count += write(1, myString++, 1);
-	return count;
+	return (count);
 }
 
 /**
@@ -75,7 +75,7 @@ int	khedira_format(char khediraspec, va_list khediraap)
 		myCount = khedira_digit((long)va_arg(khediraap, unsigned int), 16);
 	else
 		myCount += write(1, &khediraspec, 1);
-	return myCount;
+	return (myCount);
 }
 
 /**
@@ -94,10 +94,10 @@ int	khedira(const char *myFormat, ...)
 	{
 		if (*myFormat == '%')
 			count += khedira_format(*++myFormat, khedira_ap);
-		else 
+		else
 			count += write(STDOUT_FILENO, myFormat, 1);
 		++myFormat;
 	}
 	va_end(khedira_ap);
-	return count;
+	return (count);
 }
